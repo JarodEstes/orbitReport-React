7c85f39bab02878e771777b5c47ca660b6f8a73a
JarodEstes <estesjarod48@gmail.com>
2024-03-21 16:43:19 -0500

    Orbit Launch Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+
+
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
-
+import Banner from "./components/Banner";
+import {useState} from "react";
+import satData from "./components/satData";
 
 function App() {
-  return (
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+       return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+    // gets satellite data and organizes the data by type.
+    // filter function filters data passed in by orbit type and sets the data from that type to display data
+ };
+   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner />
+      <Buttons 
+      filterByType={filterByType}
+      setSat={setSat}
+      displaySats={displaySats}
+      />
+      {/* giving props for buttons */}
+      <Table sat={sat}/>
+      {/* passing sat data into table */}
     </div>
   );
 }
